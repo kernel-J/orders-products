6844028ae14482a5ac5b56883c8006a06faff633
kernel_j <jenniferwong89@gmail.com>
2023-02-25 14:51:54 -0500

    allow draggable list to accept product data

--- webapp/src/components/DraggableList/DraggableList.stories.tsx
+++ webapp/src/components/DraggableList/DraggableList.stories.tsx
@@ -1,33 +1,62 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { DragDropContext } from 'react-beautiful-dnd';
-import DraggableList from './DraggableList';
-import { Order } from '../interfaces';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { DragDropContext } from "react-beautiful-dnd";
+import DraggableList from "./DraggableList";
+import { Order, Product } from "../interfaces";
 
 export default {
-    title: 'Draggable List',
-    component: DraggableList,
+  title: "Draggable List",
+  component: DraggableList
 } as ComponentMeta<typeof DraggableList>;
 
-const Template: ComponentStory<typeof DraggableList> = (args) => (
-    <DragDropContext onDragEnd={() => {}}>
-        <DraggableList {...args} />
-    </DragDropContext>
+const Template: ComponentStory<typeof DraggableList> = args => (
+  <DragDropContext onDragEnd={() => {}}>
+    <DraggableList {...args} />
+  </DragDropContext>
 );
 
 const getArgs = (OrderStatus: string) => ({
-    ID: '12345',
-    listTitle: 'Test List',
-    removeOrder: (order: Order) => {},
-    items: [
-        { OrderID: 1234, CustomerID: 1234, ProductID: 123456, OrderStatus },
-        { OrderID: 1235, CustomerID: 1235, ProductID: 123456, OrderStatus },
-        { OrderID: 1236, CustomerID: 1236, ProductID: 123456, OrderStatus },
-    ],
+  ID: "12345",
+  listTitle: "Test List",
+  removeOrder: (item: Order) => {},
+  items: [
+    { OrderID: 1234, CustomerID: 1234, ProductID: 123456, OrderStatus },
+    { OrderID: 1235, CustomerID: 1235, ProductID: 123456, OrderStatus },
+    { OrderID: 1236, CustomerID: 1236, ProductID: 123456, OrderStatus }
+  ]
+});
+
+const getArgsProduct = () => ({
+  ID: "12345",
+  listTitle: "Prooduct Test List",
+  removeOrder: (item: Product) => {},
+  items: [
+    {
+      ProductID: 1234,
+      ProductName: "Hat",
+      ProductPhotoURL: "",
+      ProductOrder: 1
+    },
+    {
+      ProductID: 1235,
+      ProductName: "Shirt",
+      ProductPhotoURL: "",
+      ProductOrder: 2
+    },
+    {
+      ProductID: 1236,
+      ProductName: "Pants",
+      ProductPhotoURL: "",
+      ProductOrder: 3
+    }
+  ]
 });
 
 export const NotInQA = Template.bind({});
-NotInQA.args = getArgs('InProgress');
+NotInQA.args = getArgs("InProgress");
 
 export const InQA = Template.bind({});
-InQA.args = getArgs('QA');
+InQA.args = getArgs("QA");
+
+export const WithProduct = Template.bind({});
+WithProduct.args = getArgsProduct();
--- webapp/src/components/DraggableList/DraggableList.tsx
+++ webapp/src/components/DraggableList/DraggableList.tsx
@@ -1,46 +1,79 @@
-import React from 'react';
-import { DraggableProvided, Droppable, Draggable } from 'react-beautiful-dnd';
-import DraggableItem from '../DraggableItem/DraggableItem';
-import { DraggableListProps } from '../interfaces';
+import React from "react";
+import { DraggableProvided, Droppable, Draggable } from "react-beautiful-dnd";
+import DraggableItem from "../DraggableItem/DraggableItem";
+import {
+  DraggableListProps,
+  Order,
+  DraggableProductListProps,
+  Product
+} from "../interfaces";
 
-const DraggableList = (props: DraggableListProps) => (
-    <Droppable droppableId={props.ID}>
-        {(provided) => (
-            <div
-                ref={provided.innerRef}
-                className='bg-neutral-500 p-4 w-full'
-                data-testid={`droppable-container-${props.ID}`}
+const renderOrderItem = (
+  item: Order,
+  provided: DraggableProvided,
+  removeOrder: any
+) => (
+  <DraggableItem
+    OrderID={item.OrderID}
+    CustomerID={item.CustomerID}
+    ProductID={item.ProductID}
+    OrderStatus={item.OrderStatus}
+    draggableProvided={provided}
+    removeOrder={removeOrder}
+  />
+);
+
+const renderProductItem = (
+  item: Product,
+  provided: DraggableProvided,
+  removeOrder: any
+) => (
+  <DraggableItem
+    ProductID={item.ProductID}
+    ProductName={item.ProductName}
+    ProductPhotoURL={item.ProductPhotoURL}
+    ProductOrder={item.ProductOrder}
+    draggableProvided={provided}
+    removeOrder={removeOrder}
+  />
+);
+
+const DraggableList = (
+  props: DraggableListProps | DraggableProductListProps
+) => (
+  <Droppable droppableId={props.ID}>
+    {provided => (
+      <div
+        ref={provided.innerRef}
+        className="bg-neutral-500 p-4 w-full"
+        data-testid={`droppable-container-${props.ID}`}
+      >
+        <h5
+          className="font-bold text-white"
+          data-testid={`droppable-title-${props.ID}`}
+        >
+          {props.listTitle}
+        </h5>
+        {props.items.length > 0 &&
+          props.items.map((item, index) => (
+            <Draggable
+              key={"OrderID" in item ? item.OrderID : item.ProductID}
+              draggableId={`${
+                "OrderID" in item ? item.OrderID : item.ProductID
+              }`}
+              index={index}
             >
-                <h5
-                    className='font-bold text-white'
-                    data-testid={`droppable-title-${props.ID}`}
-                >
-                    {props.listTitle}
-                </h5>
-                {
-                    props.items.length > 0 && props.items.map((item, index) => (
-                        <Draggable
-                            key={item.OrderID}
-                            draggableId={`${item.OrderID}`}
-                            index={index}
-                        >
-                            {(provided: DraggableProvided) => (
-                                <DraggableItem
-                                    OrderID={item.OrderID}
-                                    CustomerID={item.CustomerID}
-                                    ProductID={item.ProductID}
-                                    OrderStatus={item.OrderStatus}
-                                    draggableProvided={provided}
-                                    removeOrder={props.removeOrder}
-                                />
-                            )}
-                        </Draggable>
-                    ))
-                }
-                {provided.placeholder}
-            </div>
-        )}
-    </Droppable>
+              {(provided: DraggableProvided) =>
+                "OrderID" in item
+                  ? renderOrderItem(item, provided, props.removeOrder)
+                  : renderProductItem(item, provided, props.removeOrder)
+              }
+            </Draggable>
+          ))}
+        {provided.placeholder}
+      </div>
+    )}
+  </Droppable>
 );
 
 export default DraggableList;
