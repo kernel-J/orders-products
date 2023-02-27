595c2c8e7c09cfb3d9127fabf48991210b80f9a2
kernel_j <jenniferwong89@gmail.com>
2023-02-26 23:21:59 -0500

    add update route to api.products

--- api.products/app/controllers/api/products_controller.rb
+++ api.products/app/controllers/api/products_controller.rb
@@ -1,9 +1,26 @@
 module Api
   class ProductsController < ApplicationController
     def index
-      product = Product.new
+      @products = Product.where("ProductStatus": "Active").order("ProductOrder")
       
-      render json: product.all
+      render json: @products, status: :ok
     end
+
+    def update
+      @product = Product.find(product_params[:id])
+      @product.assign_attributes({ProductOrder: product_params[:product_order]})
+
+      if @product.save
+        head(:ok)
+      else
+        render json: { error: 'could not save product order' }, status: :bad_request
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:id, :product_order)
+    end 
   end
 end
--- api.products/config/routes.rb
+++ api.products/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api, constraints: { format: 'json' } do
-    resources :products, only: [:index]
+    resources :products, only: [:index, :update]
   end
 end
