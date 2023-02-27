7f27d58285996979c8d57da6cb219fe86706d569
kernel_j <jenniferwong89@gmail.com>
2023-02-26 23:19:40 -0500

    add api calls for product

--- webapp/src/pages/ApiHelper.ts
+++ webapp/src/pages/ApiHelper.ts
@@ -1,49 +1,98 @@
 import axios from "axios";
-import { Order, OrderData } from "../components/interfaces";
+import { Order, OrderData, Product } from "../components/interfaces";
 
-const INPIPELINE_URL = '/api/orders/inpipeline';
+const INPIPELINE_URL = "/api/orders/inpipeline";
+const PRODUCTS_URL = "/api/products";
+
+const getProducts = async () => {
+  let productData: Array<Product> = [];
+  let errorOccured = false;
+
+  try {
+    const response = await axios.get(PRODUCTS_URL, {
+      headers: {
+        "Content-Type": "image/jpeg"
+      }
+    });
+    if (response?.status === 200) {
+      const { data } = response;
+
+      productData = [...data];
+    }
+  } catch (err) {
+    console.error(err);
+    errorOccured = true;
+  }
+  return { productData, errorOccured };
+};
+
+const updateProduct = async (product: Product) => {
+  const { ProductID, ProductOrder } = product;
+
+  try {
+    const response = await axios.put(PRODUCTS_URL + "/" + ProductID, {
+      product: {
+        id: ProductID,
+        product_order: ProductOrder
+      }
+    });
+    if (response?.status === 200) {
+      return "success";
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  return "error";
+};
 
 const getInPipelineData = async () => {
-    const orderData: OrderData = {
-      Queued: [],
-      InProgress: [],
-      QA: [],
-    };
-    let errorOccured = false;
-    try {
-      const response = await axios.get(INPIPELINE_URL);
-      if (response?.status === 200) {
-        const { data } = response.data;
-        data.forEach((order: Order) => {
-          orderData[order.OrderStatus as keyof OrderData].push(order);
-        });
-      } else {
-        const { message } = response.data;
-        throw message;
-      }
-    } catch(err) {
-      console.error(err);
-      errorOccured = true;
+  const orderData: OrderData = {
+    Queued: [],
+    InProgress: [],
+    QA: []
+  };
+  let errorOccured = false;
+  try {
+    const response = await axios.get(INPIPELINE_URL);
+    if (response?.status === 200) {
+      const { data } = response.data;
+      data.forEach((order: Order) => {
+        orderData[order.OrderStatus as keyof OrderData].push(order);
+      });
+    } else {
+      const { message } = response.data;
+      throw message;
     }
-    return { orderData, errorOccured };
+  } catch (err) {
+    console.error(err);
+    errorOccured = true;
+  }
+  return { orderData, errorOccured };
 };
 
-const UPDATE_STATUS_URL = '/api/orders/update_status';
+const UPDATE_STATUS_URL = "/api/orders/update_status";
 
 const updateOrderStatus = async (order: Order, newOrderStatus: string) => {
-    const updatedOrder = { ...order, OrderStatus: newOrderStatus };
-    let orderStatusUpdated = false;
-    try {
-        const response = await axios.post(UPDATE_STATUS_URL, updatedOrder);
-        if (response?.status === 200) orderStatusUpdated = true;
-        else {
-            const { message } = response.data;
-            throw message;
-        }
-    } catch(err) {
-        console.error(err);
+  const updatedOrder = { ...order, OrderStatus: newOrderStatus };
+  let orderStatusUpdated = false;
+  try {
+    const response = await axios.post(UPDATE_STATUS_URL, updatedOrder);
+    if (response?.status === 200) orderStatusUpdated = true;
+    else {
+      const { message } = response.data;
+      throw message;
     }
-    return orderStatusUpdated;
+  } catch (err) {
+    console.error(err);
+  }
+  return orderStatusUpdated;
 };
 
-export { getInPipelineData, INPIPELINE_URL, updateOrderStatus, UPDATE_STATUS_URL };
+export {
+  getInPipelineData,
+  INPIPELINE_URL,
+  updateOrderStatus,
+  UPDATE_STATUS_URL,
+  getProducts,
+  updateProduct
+};
