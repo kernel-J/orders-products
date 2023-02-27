073efae556c5559cd5ffdaf311dfdfbf6380b77d
kernel_j <jenniferwong89@gmail.com>
2023-02-26 23:21:09 -0500

    add products to productpage

--- webapp/src/pages/ProductsPage/ProductsPage.tsx
+++ webapp/src/pages/ProductsPage/ProductsPage.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import PageWrapper from '../PageWrapper';
+import React, { useEffect, useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import DraggableList from "../../components/DraggableList/DraggableList";
+import PageWrapper from "../PageWrapper";
+import { getProducts, updateProduct } from "../ApiHelper";
+import { Product } from "../../components/interfaces";
+import { calculateProductOrder, reorderProducts } from "./Utils";
+import Spinner from "../../components/Spinner/Spinner";
 
 const ProductsPage = () => {
   /*
@@ -8,13 +14,114 @@ const ProductsPage = () => {
       Instead of modifying the data locally we want to do it serverside via a post
       request
   */
-  return (
-    <PageWrapper>
-      <h1 className="text-3xl font-bold text-white">
-        Product Page Goes Here
-      </h1>
-    </PageWrapper>
-  );
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([] as Array<Product>);
+  const [error, setError] = useState("");
+
+  const handleDragEnd = (result: any) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    const srcIndex = source.index;
+    const destIndex = destination.index;
+
+    if (srcIndex === destIndex) return;
+
+    const product = products[srcIndex];
+    const newProductOrder = calculateProductOrder(
+      products,
+      destIndex,
+      srcIndex
+    );
+
+    if (newProductOrder === -1) {
+      setError("An error has occurred");
+    } else {
+      product.ProductOrder = newProductOrder;
+
+      const newProducts = reorderProducts(
+        products,
+        product,
+        destIndex,
+        srcIndex
+      );
+      setProducts(newProducts);
+      update(product);
+    }
+  };
+
+  const update = async (product: Product) => {
+    setIsLoading(true);
+    const result = await updateProduct(product);
+    setIsLoading(false);
+    if (result === "error") {
+      setError("An error has occurred");
+    }
+  };
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    const results = await getProducts();
+    setIsLoading(false);
+    if (results.errorOccured) {
+      setError("An error has occurred");
+    } else {
+      setProducts(results.productData);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  let content;
+  if (isLoading) {
+    content = (
+      <div
+        className="flex flex-row justify-center w-full pt-4"
+        data-testid="loading-spinner-container"
+      >
+        <Spinner />
+      </div>
+    );
+  } else {
+    if (error) {
+      content = (
+        <div
+          className="flex flex-row justify-center w-full pt-4 text-3xl font-bold text-white"
+          data-testid="error-container"
+        >
+          {error}
+        </div>
+      );
+    } else if (products.length > 0) {
+      content = (
+        <div
+          className="flex flex-row justify-center w-full pt-4"
+          data-testid="products-container"
+        >
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <DraggableList
+              ID="0"
+              listTitle="Products"
+              removeOrder={(item: Product) => {}}
+              items={products}
+            />
+          </DragDropContext>
+        </div>
+      );
+    } else {
+      content = (
+        <h1 className="text-3xl font-bold text-white">
+          {"There are no products available"}
+        </h1>
+      );
+    }
+  }
+
+  return <PageWrapper>{content}</PageWrapper>;
 };
 
-export default ProductsPage
+export default ProductsPage;
